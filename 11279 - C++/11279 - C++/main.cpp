afb7892be53b33f6582079fe560c7bf5b6e355bd
iceHood <hanyo3477@gmail.com>
2021-12-25 17:24:24 +0900

    dd
    ??

--- 11279 - C++/11279 - C++/main.cpp
+++ 11279 - C++/11279 - C++/main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+int N, x;
+priority_queue<int> q;
 int main() {
-    
+    scanf("%d", &N);
+    while (N--) {
+        scanf("%d", &x);
+        if (x == 0) {
+            if (!q.empty()) {
+                cout << q.top() << "\n";
+                q.pop();
+            } else cout << 0 << "\n";
+        } else q.push(x);
+    }
 }
