ab3640edbd3e7f7c4b347886aec3a3202967b7d6
임정현 <hanyo3477@gmail.com>
2023-01-11 14:52:39 +0900

    Implementation
    Implementation

--- Algorithm_Swift/Algorithm_Swift/13. Roman to Integer.swift
+++ Algorithm_Swift/Algorithm_Swift/13. Roman to Integer.swift
@@ -1,53 +1,22 @@
 //class Solution {
-//    func intToRoman(_ num: Int) -> String {
-//        var answer = ""
-//        var n = num
-//        while n > 0 {
-//            if 1000... ~= n {
-//                answer += "M"
-//                n -= 1000
-//            } else if 900..<1000 ~= n {
-//                answer += "CM"
-//                n -= 900
-//            } else if 500..<900 ~= n {
-//                answer += "D"
-//                n -= 500
-//            } else if 400..<500 ~= n {
-//                answer += "CD"
-//                n -= 400
-//            } else if 100..<400 ~= n {
-//                answer += "C"
-//                n -= 100
-//            } else if 90..<100 ~= n {
-//                answer += "XC"
-//                n -= 90
-//            } else if 50..<90 ~= n {
-//                answer += "L"
-//                n -= 50
-//            } else if 40..<50 ~= n {
-//                answer += "XL"
-//                n -= 40
-//            } else if 10..<40 ~= n {
-//                answer += "X"
-//                n -= 10
-//            } else if 9..<10 ~= n {
-//                answer += "IX"
-//                n -= 9
-//            } else if 5..<9 ~= n {
-//                answer += "V"
-//                n -= 5
-//            } else if 4..<5 ~= n {
-//                answer += "IV"
-//                n -= 4
-//            } else if 1..<4 ~= n {
-//                answer += "I"
-//                n -= 1
+//
+//    let symbols: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
+//
+//    func romanToInt(_ s: String) -> Int {
+//        var answer = 0
+//        var prev = 1001
+//        for ch in s {
+//            guard let temp = symbols[ch] else {continue}
+//            if prev < temp {
+//                answer += temp - 2 * prev
+//                prev = temp - prev
+//            } else {
+//                answer += temp
+//                prev = temp
 //            }
 //        }
 //        return answer
 //    }
 //}
 //
-//
-//var ff = Solution()
-//print(ff.intToRoman(1994))
+//print(Solution().romanToInt("MCMXCIV"))
