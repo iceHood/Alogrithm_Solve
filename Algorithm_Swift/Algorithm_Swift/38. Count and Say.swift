b901955484286a5ecb09bb511607c1e93b45ca16
임정현 <hanyo3477@gmail.com>
2023-02-04 10:59:39 +0900

    Implementation
    Implementation

---
+++ Algorithm_Swift/Algorithm_Swift/38. Count and Say.swift
@@ -0,0 +1,22 @@
+//class Solution {
+//    func countAndSay(_ n: Int) -> String {
+//        if n == 1 {
+//            return "1"
+//        }
+//        var result = ""
+//        var temp = ""
+//        for i in countAndSay(n - 1) {
+//            if temp.isEmpty || temp.last! == i {
+//                temp += "\(i)"
+//            } else {
+//                result += "\(temp.count)\(temp.last!)"
+//                temp = "\(i)"
+//            }
+//        }
+//        result += "\(temp.count)\(temp.last!)"
+//        return result
+//    }
+//}
+//
+//print(Solution().countAndSay(6))
+//
