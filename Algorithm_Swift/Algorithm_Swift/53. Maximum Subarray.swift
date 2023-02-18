33ae500f73dee41262449a385c9f88ed76b07d14
임정현 <hanyo3477@gmail.com>
2023-02-18 14:47:00 +0900

    Implementation
    Implementation

---
+++ Algorithm_Swift/Algorithm_Swift/53. Maximum Subarray.swift
@@ -0,0 +1,17 @@
+//class Solution {
+//    func maxSubArray(_ nums: [Int]) -> Int {
+//        var maxNum = nums[0]
+//        nums.reduce(0) { partialResult, num in
+//            maxNum = max(maxNum, partialResult + num, num)
+//            return max(partialResult + num, num)
+//        }
+//        return maxNum
+//    }
+//}
+//
+////var nums = [-2,1,-3,4,-1,2,1,-5,4]
+////var nums = [1]
+////var nums = [5,4,-1,7,8]
+//var nums = [-2,1,-3,4,-1,2,1,-5,432]
+//
+//print(Solution().maxSubArray(nums))
