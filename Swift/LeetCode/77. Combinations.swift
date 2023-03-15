f42f23aaaa11a540b14c838f88938f48c4783028
임정현 <hanyo3477@gmail.com>
2023-03-15 23:04:31 +0900

    Combination
    Combination

---
+++ Swift/LeetCode/77. Combinations.swift
@@ -0,0 +1,25 @@
+//class Solution {
+//    func combine(_ n: Int, _ k: Int) -> [[Int]] {
+//        var result = [[Int]]()
+//        var temp = [Int]()
+//        func recur(_ start: Int) {
+//            if temp.count == k {
+//                result.append(temp)
+//            } else {
+//                guard start <= n else {return}
+//                for i in start...n {
+//                    temp.append(i)
+//                    recur(i + 1)
+//                    temp.popLast()
+//                }
+//            }
+//        }
+//        recur(1)
+//        return result
+//    }
+//}
+//
+////var  n = 4, k = 2
+////var  n = 1, k = 1
+//var  n = 14, k = 10
+//print(Solution().combine(n, k))
