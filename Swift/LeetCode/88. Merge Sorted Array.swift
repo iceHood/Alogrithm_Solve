2b3499035e163a29206c76558afbfa3a6f510a97
iceHood <hanyo3477@gmail.com>
2023-03-26 22:51:11 +0900

    Implementation
    Implementation

---
+++ Swift/LeetCode/88. Merge Sorted Array.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var nums1Idx = m
+        var nums2Idx = 0
+        while nums1Idx < nums1.count {
+            nums1[nums1Idx] = nums2[nums2Idx]
+            nums1Idx += 1
+            nums2Idx += 1
+        }
+        nums1.sort()
+    }
+}
