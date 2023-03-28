bb9bcdfdf8364311a73d2b6c60ffb7f60a3ea9e5
iceHood <hanyo3477@gmail.com>
2023-03-28 09:41:37 +0900

    BackTracking

---
+++ Swift/LeetCode/90. Subsets II.swift
@@ -0,0 +1,24 @@
+//class Solution {
+//    func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+//        var sets: Set<[Int]> = [[]]
+//        var nums = nums.sorted()
+//        var temp: [Int] = []
+//        func backTrack(start: Int) {
+//            if start == nums.count {
+//                return
+//            }
+//            for i in start..<nums.count {
+//                temp.append(nums[i])
+//                sets.insert(temp)
+//                backTrack(start: i + 1)
+//                temp.popLast()
+//            }
+//        }
+//        backTrack(start: 0)
+//        return Array(sets)
+//    }
+//}
+//
+//var nums = [1,2,2]
+//print(Solution().subsetsWithDup(nums))
+//
