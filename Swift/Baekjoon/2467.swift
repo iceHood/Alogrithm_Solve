4df540ca07f990ed386bdd200a0d6792a7a06395
iceHood <hanyo3477@gmail.com>
2023-06-29 20:49:08 +0900

    two pointers

---
+++ Swift/Baekjoon/2467.swift
@@ -0,0 +1,27 @@
+//let N = Int(readLine()!)!
+//var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+//
+//var leftIdx = 0 // left idx
+//var rightIdx = arr.count - 1 // right idx
+//
+//let target = 0
+//var sum = abs(arr[leftIdx] + arr[rightIdx])
+//
+//var answer = (left: arr[leftIdx], right: arr[rightIdx]) // answer left and right number
+//
+//while leftIdx < rightIdx {
+//    let temp = arr[leftIdx] + arr[rightIdx]
+//    if abs(temp) < sum {
+//        sum = abs(temp)
+//        answer.left = arr[leftIdx]
+//        answer.right = arr[rightIdx]
+//    }
+//
+//    if temp < 0 {
+//        leftIdx += 1
+//    } else {
+//        rightIdx -= 1
+//    }
+//}
+//
+//print(answer.left, answer.right)
