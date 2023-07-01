fb7be4415bc593cea6b77f09b85ffe73a6893610
iceHood <hanyo3477@gmail.com>
2023-07-01 18:48:52 +0900

    topology sort

---
+++ Swift/2252.swift
@@ -0,0 +1,37 @@
+//let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let (N, M) = (NM[0], NM[1])
+//var graph = [[Int]](repeating: [Int](), count: N+1)
+//var degree = [Int](repeating: 0, count: N+1)
+//var queue = [Int]()
+//var answer = [Int]()
+//for _ in 0..<M {
+//    let read = readLine()!.split(separator: " ").map { Int(String($0))! }
+//    let (a, b) = (read[0], read[1])
+//    graph[a].append(b)
+//    degree[b] += 1
+//}
+//
+//for i in 1...N {
+//    if degree[i] == 0 {
+//        queue.append(i)
+//    }
+//}
+//
+//queue.reverse()
+//var nextQueue = [Int]()
+//
+//while !queue.isEmpty {
+//    let tmp = queue.popLast()!
+//    answer.append(tmp)
+//    for i in graph[tmp] {
+//        degree[i] -= 1
+//        if degree[i] == 0 {
+//            nextQueue.append(i)
+//        }
+//    }
+//    if queue.isEmpty {
+//        queue = [Int](nextQueue.reversed())
+//        nextQueue = []
+//    }
+//}
+//answer.forEach { print($0,terminator: " ") }
