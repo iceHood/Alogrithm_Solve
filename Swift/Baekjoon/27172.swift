0fd2e332226462d752863ff99f2af79d7c16c3c5
iceHood <hanyo3477@gmail.com>
2023-07-11 11:17:20 +0900

    sosu

---
+++ Swift/Baekjoon/27172.swift
@@ -0,0 +1,18 @@
+//let N = Int(readLine()!)!
+//let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+//var answer = [Int](repeating: 0, count: 1000001)
+//var isHas = [Bool](repeating: false, count: 1000001)
+//arr.forEach { num in
+//    isHas[num] = true
+//}
+//for i in 0..<N {
+//    for j in stride(from: arr[i]*2, through: 1000000, by: arr[i]) {
+//        if isHas[j] {
+//            answer[arr[i]] += 1
+//            answer[j] -= 1
+//        }
+//    }
+//}
+//var str = ""
+//arr.forEach({ str += "\(answer[$0]) " })
+//print(str)
