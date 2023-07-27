77804eafe4b162fe45ece6377ec7494cd4ad7093
iceHood <hanyo3477@gmail.com>
2023-07-27 17:44:30 +0900

    pascal

---
+++ Swift/LeetCode/118. Pascal's Triangle.swift
@@ -0,0 +1,18 @@
+//class Solution {
+//    func generate(_ numRows: Int) -> [[Int]] {
+//        var answer = [[1]]
+//        for i in 1..<numRows {
+//            var temp = [Int]()
+//            for j in 0...i {
+//                if j == 0 || j == i {
+//                    temp.append(1)
+//                }
+//                else {
+//                    temp.append(answer[i-1][j-1] + answer[i-1][j])
+//                }
+//            }
+//            answer.append(temp)
+//        }
+//        return answer
+//    }
+//}
