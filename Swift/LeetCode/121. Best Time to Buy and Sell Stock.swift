cb5809a8267804f0e7ca2bf192bde203078de9a2
iceHood <hanyo3477@gmail.com>
2023-08-01 09:37:47 +0900

    greedy

---
+++ Swift/LeetCode/121. Best Time to Buy and Sell Stock.swift
@@ -0,0 +1,18 @@
+//class Solution {
+//    func maxProfit(_ prices: [Int]) -> Int {
+//        var base = prices[0]
+//        var answer = 0
+//        for i in 1..<prices.count {
+//            if prices[i] < base {
+//                base = prices[i]
+//            }
+//            answer = max(prices[i]-base, answer)
+//        }
+//        return answer
+//    }
+//}
+//
+//var prices = [7,1,5,3,6,4]
+//print(Solution().maxProfit(prices))
+//prices = [7,6,4,3,1]
+//print(Solution().maxProfit(prices))
