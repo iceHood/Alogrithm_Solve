ab3e4661069a02143545391575e4f0063548a92a
iceHood <hanyo3477@gmail.com>
2023-08-17 10:10:42 +0900

    Update 134. Gas Station.swift

--- Swift/LeetCode/134. Gas Station.swift
+++ Swift/LeetCode/134. Gas Station.swift
@@ -7,7 +7,7 @@
 //        var answer = 0
 //        
 //        let nextPos = { now in (now + 1) % gas.count }
-//        
+//
 //        while step <= gas.count {
 //            if cycle > gas.count { return -1 }
 //            if gasTank < cost[nowPos] {
@@ -23,7 +23,7 @@
 //            gasTank += gas[nowPos]
 //            step += 1
 //        }
-//        
+//
 //        return answer
 //    }
 //}
