80ef9301d390bd99afd7c33e22cab97e25987b39
iceHood <hanyo3477@gmail.com>
2023-08-29 09:54:22 +0900

    linked list

---
+++ Swift/LeetCode/148. Sort List.swift
@@ -0,0 +1,28 @@
+//public class ListNode {
+//    public var val: Int
+//    public var next: ListNode?
+//    public init() { self.val = 0; self.next = nil; }
+//    public init(_ val: Int) { self.val = val; self.next = nil; }
+//    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+//}
+//
+//class Solution {
+//    func sortList(_ head: ListNode?) -> ListNode? {
+//        var arr = [ListNode]()
+//        var temp = head
+//        while temp != nil {
+//            let prev = temp
+//            arr.append(temp!)
+//            temp = temp!.next
+//            prev!.next = nil
+//        }
+//        temp = ListNode()
+//        var temp2 = temp!
+//        _=arr.sorted(by: {$0.val < $1.val}).reduce(into: temp2) { partialResult, node in
+//            partialResult.next = node
+//            partialResult = partialResult.next!
+//        }
+//        return temp!.next
+//    }
+//}
+//
