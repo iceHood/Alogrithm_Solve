a7e3c0bcf0fa008d9d96b481d1bb351779ba0771
iceHood <hanyo3477@gmail.com>
2023-08-30 13:34:46 +0900

    data structure

---
+++ Swift/LeetCode/150. Evaluate Reverse Polish Notation.swift
@@ -0,0 +1,28 @@
+//class Solution {
+//    func evalRPN(_ tokens: [String]) -> Int {
+//        var stack = [Int]()
+//        tokens.forEach { token in
+//            switch token {
+//            case "+":
+//                let num2 = stack.removeLast()
+//                let num1 = stack.removeLast()
+//                stack.append(num1 + num2)
+//            case "-":
+//                let num2 = stack.removeLast()
+//                let num1 = stack.removeLast()
+//                stack.append(num1 - num2)
+//            case "*":
+//                let num2 = stack.removeLast()
+//                let num1 = stack.removeLast()
+//                stack.append(num1 * num2)
+//            case "/":
+//                let num2 = stack.removeLast()
+//                let num1 = stack.removeLast()
+//                stack.append(num1 / num2)
+//            default:
+//                stack.append(.init(token)!)
+//            }
+//        }
+//        return stack[0]
+//    }
+//}
