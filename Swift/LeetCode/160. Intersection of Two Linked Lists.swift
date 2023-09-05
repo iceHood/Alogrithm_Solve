db019d73392ac30af5c9085799d9ab1b9584a0e5
iceHood <hanyo3477@gmail.com>
2023-09-05 10:15:00 +0900

    Linked List

---
+++ Swift/LeetCode/160. Intersection of Two Linked Lists.swift
@@ -0,0 +1,27 @@
+//public class ListNode {
+//    public var val: Int
+//    public var next: ListNode?
+//    public init(_ val: Int) {
+//        self.val = val
+//        self.next = nil
+//    }
+//}
+//
+//
+//class Solution {
+//    func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+//        var set: Set<UnsafeMutableRawPointer> = []
+//        var headA = headA, headB = headB
+//        while headA != nil {
+//            set.insert(Unmanaged.passUnretained(headA!).toOpaque())
+//            headA = headA?.next
+//        }
+//        while headB != nil {
+//            if set.contains(Unmanaged.passUnretained(headB!).toOpaque()) {
+//                return headB
+//            }
+//            headB = headB?.next
+//        }
+//        return nil
+//    }
+//}
